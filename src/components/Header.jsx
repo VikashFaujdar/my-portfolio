a8049ea758c3198a21c6468fc933c8fb7732b193
vikash faujdar <vk7571027@gmail.com>
2025-09-11 04:45:36 +0530

    Header animation complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,24 @@ import { Link } from 'react-router-dom'
 import { RiMenu3Line } from "react-icons/ri";
 import { IoCloseSharp } from "react-icons/io5";
 import gsap from 'gsap';
+import { SplitText } from 'gsap/SplitText';
 
 
 const Header = () => {
 
     const [showToggle, setshowToggle] = useState(true)
+    const SplittedLogo = SplitText.create(".logoName",{
+        type:"chars",
+    })
+
+    
+
+    gsap.from(SplittedLogo.chars,{
+        y:20,
+        opacity:0,
+        duration:0.1,
+        stagger:0.2
+    })
     
     
     const handleToggle = () => {
@@ -33,19 +46,31 @@ const Header = () => {
     }
 
   return (
-    <div className='bg-[#1E1E1E] text-white flex items-center relative'>
+    <div className='bg-[#1E1E1E] header text-white flex items-center relative'>
         <div className="container h-15 flex items-center justify-between px-4 lg:px-0 py-5">
             <div className="logo">
                 <Link to={'/'}>
-                <h2>Vikash Kumar</h2>
+                <h2 className='font-primary logoName font-medium text-xl hover:text-black relative hover:bg-transparent group overflow-hidden z-10 transition-all duration-500'>Vikash Kumar
+                    <div className="absolute h-full w-full bg-zinc-300 top-0 -left-34 -z-10 group-hover:left-0 transition-all duration-500"></div>
+                </h2>
                 </Link>
             </div>
-            <div className="middle hidden  md:flex items-center justify-center gap-10 font-secondary text-sm">
-                <Link className='hover:text-blue-500 transition-all duration-300' to={'/'}>home</Link>
-                <Link className='hover:text-blue-500 transition-all duration-300' to={'/about'}>About</Link>
-                <Link className='hover:text-blue-500 transition-all duration-300' to={'/gallery'}>Gallery</Link>
-                <Link className='hover:text-blue-500 transition-all duration-300' to={'/projects'}>Projects</Link>
-                <Link className='hover:text-blue-500 transition-all duration-300' to={'/contact'}>Contact</Link>
+            <div className="middle hidden  md:flex items-center justify-center gap-15 font-secondary text-md">
+                <Link className=' relative transition-all duration-300 overflow-hidden group' to={'/'}>Home
+                <div className="underLine absolute w-full h-full border-b-2 border-white top-0 -left-18 group-hover:left-0 transition-all duration-300"></div>
+                </Link>
+                <Link className=' relative transition-all duration-300 overflow-hidden group' to={'/about'}>About
+                <div className="underLine absolute w-full h-full border-b-2 border-white top-0 -left-18 group-hover:left-0 transition-all duration-300"></div>
+                </Link>
+                <Link className=' relative transition-all duration-300 overflow-hidden group' to={'/gallery'}>Gallery
+                <div className="underLine absolute w-full h-full border-b-2 border-white top-0 -left-18 group-hover:left-0 transition-all duration-300"></div>
+                </Link>
+                <Link className=' relative transition-all duration-300 overflow-hidden group' to={'/projects'}>Projects
+                <div className="underLine absolute w-full h-full border-b-2 border-white top-0 -left-18 group-hover:left-0 transition-all duration-300"></div>
+                </Link>
+                <Link className=' relative transition-all duration-300 overflow-hidden group' to={'/contact'}>Contact
+                <div className="underLine absolute w-full h-full border-b-2 border-white top-0 -left-18 group-hover:left-0 transition-all duration-300"></div>
+                </Link>
             </div>
             <div className="h-full mobnav w-full bg-black fixed top-15 py-10 -left-[100%] md:hidden font-primary flex flex-col pl-4 gap-9 text-5xl">
                 <Link onClick={handleToggle} className='Links overflow-hidden' to={'/'}>Home</Link>
@@ -55,7 +80,10 @@ const Header = () => {
                 <Link onClick={handleToggle} className='Links overflow-hidden' to={'/contact'}>Contact</Link>
             </div>
             <div className="right flex items-center gap-6">
-               <Link to={'/contact'} ><button className='cursor-pointer border-1 border-zinc-500 px-4 py-1'>Hire Me</button></Link>
+               <Link to={'/contact'} ><button className='relative cursor-pointer border-1 border-zinc-500 px-4 md:px-5 py-1 bg-white text-black hover:text-white text-md transition-all group duration-300 overflow-hidden z-20 rounded-sm hover:bg-transparent'>Hire Me
+                <div className="btn-animation absolute w-full h-full group-hover:bottom-0 bg-black -z-10 -bottom-8 transition-all duration-500 left-0"></div>
+                <div className="btn-animation-2 absolute w-full h-full group-hover:top-0 bg-black -z-10 -top-8 transition-all duration-500 left-0"></div>
+                </button></Link>
                {
                    showToggle == true ? <RiMenu3Line className='md:hidden size-8' onClick={handleToggle}/>: <IoCloseSharp className='md:hidden size-8' onClick={handleToggle} />
                }
