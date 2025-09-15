abdd1be2787537e1b298cf25e239e90432482eaf
vikash faujdar <vk7571027@gmail.com>
2025-09-15 22:56:43 +0530

    add call to action section in the contact us page

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ContactUs from '../../components/ContactUs'
+import CallToAction from '../../components/CallToAction'
 
 const Contact = () => {
   return (
     <div className='py-20 md:py-40'>
       <ContactUs/>
+      <CallToAction/>
     </div>
   )
 }
