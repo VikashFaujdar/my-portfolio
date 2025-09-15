35553a9599913ec9c9f0bfccae54887cb30a805c
vikash faujdar <vk7571027@gmail.com>
2025-09-15 22:52:31 +0530

    add my images properly

--- src/Pages/About/Hero.jsx
+++ src/Pages/About/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
                 <h1 className='text-5xl md:text-[6vw] lg:text-5xl font-primary font-medium'>Who I am beyond Design</h1>
                 <div className="sections md:flex md:gap-8 lg:mt-5">
                     <div className="left my-3 md:w-1/2">
-                        <img src={myfoto} alt="image" className='max-h-120 w-full object-cover object-top' />
+                        <img src={myfoto} alt="image" className='max-h-120 w-full object-cover object-top rounded-xl' />
                     </div>
                     <div className="left flex flex-col lg:gap-3 md:w-1/2">
                         <h2 className='text-2xl lg:text-3xl font-secondary font-medium my-3'>More than a UX/UI designer, I'm a storyteller at heart, weaving narratives through my designs.</h2>
