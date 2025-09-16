cd6a1fdb50b31e8247da799a528e04abdd1471d6
vikash faujdar <vk7571027@gmail.com>
2025-09-16 13:48:11 +0530

    add battery

--- src/components/BatteryIndicator.jsx
+++ src/components/BatteryIndicator.jsx
@@ -18,7 +18,7 @@ const BatteryIndicator = () => {
    });
 
   return (
-    <div className='rounded-tr-md rounded-br-md w-12 bg-white h-5 relative  rounded-sm  flex items-center justify-center'>
+    <div className='hidden rounded-tr-md rounded-br-md w-12 bg-white h-5 relative  rounded-sm  md:flex items-center justify-center'>
         <h1 className='text-black text-sm font-primary font-medium relative z-10'>{batterlevel}%</h1>
     <div className="line w-1 absolute -right-[3px] rounded-tr-[2px] rounded-br-[2px] h-3 bg-white"></div>
     </div>
