62ba82aa5e46a234e618c6527cff47c5f88e8f5f
vikash faujdar <vk7571027@gmail.com>
2025-09-17 22:55:35 +0530

    okar sir!

--- src/Pages/Home/SelectedProjects.jsx
+++ src/Pages/Home/SelectedProjects.jsx
@@ -12,8 +12,8 @@ const SelectedProjects = () => {
         const SelectedTl = gsap.timeline({
             scrollTrigger:{
                 trigger:SelectedRef.current,
-                start:"top 60%",
-                end:"bottom 80%",
+                start:"top 90%",
+                end:"bottom 100%",
                 scrub:true,
             }
         });
