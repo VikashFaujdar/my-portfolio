656241842402fa6a548289efec41c16eb932b894
VikashFaujdar <vk7571027@gmail.com>
2025-11-10 10:54:46 +0530

    adding all projects links

--- src/Pages/Projects/Hero.jsx
+++ src/Pages/Projects/Hero.jsx
@@ -7,7 +7,7 @@ import gsap from 'gsap';
 const Hero = () => {
 
     const [show, setShow] = useState(false)
-
+    
     const Projects = [
         {
             Image: card1,
@@ -23,10 +23,28 @@ const Hero = () => {
         },
         {
             Image: grade,
+            Title: "Redesign project: Miracles Health Care",
+            subTitle: "Creating the front-end for the miracles health care hospital at dixinfotech sevices llp.",
+            link:`https://www.miracleshealth.com/`
+        },
+        {
+            Image: grade,
             Title: "Redesign project: Lakshya Computer Coaching",
             subTitle: "Revolutionizing the educational website designed to enhance computer knowledge interactive learning and peer collaboration.",
             link:`https://computer-classes-vikash-fauzdar.vercel.app/`
-        }
+        },
+        {
+            Image: grade,
+            Title: "Redesign project: Sundown Studio",
+            subTitle: "Creating the clone of sundown studio and increase my knowledge how to do responsive or many more things related to the css.",
+            link:`https://sundownstudio-by-vikashkumar.netlify.app/`
+        },
+        {
+            Image: grade,
+            Title: "Redesign project: Usability Hub",
+            subTitle: "Creating the clone of Usability hub and increase my knowledge how to do responsive or many more things related to the css.",
+            link:`https://usability-hub-by-vikash-kumar.vercel.app/`
+        },
     ]
 
 
@@ -42,11 +60,11 @@ const Hero = () => {
                         Projects.map((elem, idx) => {
                             return (
                                 <div key={idx} className="card group flex bg-transparent flex-col overflow-hidden rounded-md">
-                                    <a href={elem.link} target="_blank" rel="noopener noreferrer" className='relative group overflow-hidden'>
-                                        <div className="circle h-20 w-20 hidden rounded-full text-black text-xl top-[30%] left-[30%] bg-white absolute translate-x-1/2 translate-y-1/2 z-30 group-hover:flex items-center justify-center font-primary hover:bg-black hover:text-white transition-all duration-300">
+                                    <a href={elem.link} target="_blank" rel="noopener noreferrer" className='relative group rounded-xl overflow-hidden w-full'>
+                                        <div className="circle h-20 w-20 hidden rounded-full text-black text-xl top-[30%] left-[30%] md:left-[40%] md:top-[30%] bg-white absolute translate-x-1/2 translate-y-1/2 z-30 group-hover:flex items-center justify-center font-primary hover:bg-black hover:text-white transition-all duration-300">
                                             <h1>open</h1>
                                         </div>
-                                    <img src={elem.Image} alt="card Image" className='rounded-xl h-100 group-hover:scale-105 transition-all duration-300 object-cover object-center' />
+                                    <img src={elem.Image} alt="card Image" className='rounded-xl h-100 w-full group-hover:scale-105 transition-all duration-300 object-cover object-center' />
                                     </a>
                                     <div className="dts flex flex-col gap-2 py-3">
                                         <h1 className='text-2xl sm:text-[5vw] md:text-[4vw] lg:text-4xl font-secondary font-medium'>{elem.Title}</h1>
