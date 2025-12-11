b1857b74dacea48c7a79b02cc0e34e4ca576fb60
VikashFaujdar <vk7571027@gmail.com>
2025-12-11 13:46:18 +0530

    bettery indicator right  side modify

--- src/components/BatteryIndicator.jsx
+++ src/components/BatteryIndicator.jsx
@@ -36,7 +36,7 @@ export default function BatteryStatus() {
     <div className='hidden rounded-tr-md rounded-br-md w-12 group transition-all duration-200 bg-white hover:bg-zinc-500 text-white h-5 relative  rounded-sm  md:flex items-center justify-center'>
       <h1 className='text-black group-hover:text-white transition-all duration-200 text-sm font-primary font-medium relative z-10'>{batteryLevel}%</h1>
       <BsFillLightningChargeFill className={charging == true ? 'block text-green-400' : 'hidden'} size={13}/>
-      <div className="line w-2 absolute -right-[2px] rounded-tr-[2px] group-hover:bg-zinc-500 transition-all duration-200 rounded-br-[2px] h-3 bg-white"></div>
+      <div className="line w-1 absolute -right-[3px] rounded-tr-[2px] group-hover:bg-zinc-500 transition-all duration-200 rounded-br-[2px] h-3 bg-white"></div>
     </div>
   );
 }
